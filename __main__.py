4779f73300090c89c27fdf86eaaf75ac9502403d
kahootKing <alyssa.mer14@gmail.com>
2026-03-10 21:29:21 -0400

    Update Main Format

--- __main__.py
+++ __main__.py
@@ -2,10 +2,14 @@
 #
 # Developed by: Alyssa Merante (alyssa.merante@gmail.com)
 #
-# The purpose of this application is to provide a method of generating medical imaging test files (DICOM & HL7)  to be used for testing purposes only.
+# The purpose of this application is to provide a method of generating anonymized medical imaging test files (DICOM & HL7)  to be used for testing purposes only.
 # 
 # ** IMPORTANT ** It is the responsibility of the user to generate test files that do not violate HIPPA by using identifiable patient information.
 
 
-# (1) Initialize Application
-import initialize
+# Initialize Application
+def main():
+    import initialize
+
+if __name__ == "__main__":
+    main()

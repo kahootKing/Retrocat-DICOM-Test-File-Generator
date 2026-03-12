2f27165456a59fc117ef82e12cb6f870d10dbc51
kahootKing <alyssa.mer14@gmail.com>
2026-03-11 21:33:18 -0400

    Add Code Ownership Information to __main__
    Adding comments which state that this code was developed by Alyssa Merante as a personal project to grow technical skills and is not owned by any business entity.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,13 @@
 ## Retrocat: Medical Imaging Test File Generator
 #
-# Developed by: Alyssa Merante (alyssa.merante@gmail.com)
+# Developed by: Alyssa N. Merante (alyssa.mer14@gmail.com), aka KahootKing (or DuckDuckTheKahootKing)
 #
-# The purpose of this application is to provide a method of generating anonymized medical imaging test files (DICOM & HL7)  to be used for testing purposes only.
-# 
+# The purpose of this application is to provide a method of generating anonymized medical imaging test files (DICOM & HL7) to be used for testing purposes only.
 # ** IMPORTANT ** It is the responsibility of the user to generate test files that do not violate HIPPA by using identifiable patient information.
+#
+# This application was developed as a fun & personal side-project and is not owned by any business entity. This application was strictly developed on personal time.
+# This open-source code is available free-of-charge to anyone who would like to use it. 
+#
 
 from os import getcwd
 
